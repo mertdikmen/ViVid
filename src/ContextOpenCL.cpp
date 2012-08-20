315a64d8803388986aa589aac2edd356157863d9
antonio j dios <antjgm@gmail.com>
2012-08-20 15:52:56 -0500

    adding cpu context

--- src/ContextOpenCL.cpp
+++ src/ContextOpenCL.cpp
@@ -9,16 +9,34 @@
 
 #include "ContextOpenCL.h"
 
-myContexOpenCl *  TheContext::The_Context_Singleton=NULL;
+myContexOpenCl *  TheContext::The_Context_GPU=NULL;
+myContexOpenCl *  TheContext::The_Context_CPU=NULL;
+
+
 
 TheContext::TheContext()
 {
-	if (The_Context_Singleton==NULL){
+	if (The_Context_GPU==NULL){
 	//	printf("Is NULL");
-		The_Context_Singleton = new myContexOpenCl();
+		The_Context_GPU = new myContexOpenCl( CL_DEVICE_TYPE_GPU );
+		printf("\nGPU context done\n\n");
 	}
+	
+	if (The_Context_CPU==NULL){
+		// the_Contexts[0]=The_Context_Singleton;
+		 The_Context_CPU= new myContexOpenCl( CL_DEVICE_TYPE_CPU );
+		 printf("\nCPU context done\n\n");
+
+	}
+
+	
+
 }
 
 myContexOpenCl * TheContext::getMyContext(){
-	return The_Context_Singleton;
+	return The_Context_CPU;
 }
+myContexOpenCl * TheContext::getMyContextCPU(){
+	return  The_Context_CPU;
+}
+
