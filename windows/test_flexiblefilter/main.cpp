859dc0de19eee18183d14120acacf704f973cffd
Mert Dikmen <mertdikmen@gmail.com>
2012-10-02 22:01:50 -0500

    Added new test project

--- windows/test_flexiblefilter/main.cpp
+++ windows/test_flexiblefilter/main.cpp
@@ -16,21 +16,5 @@ int main(int argc, char* argv[])
 	const int height = exampleImage.size().height;
 	const int width = exampleImage.size().width;
 
-	//create a device matrix
-	DeviceMatrixCL::Ptr dmpCL = makeDeviceMatrixCL(height, width);
-
-	//copy to the DeviceMatrix
-	DeviceMatrixCL_copyToDevice(*dmpCL, f_imData);
-
-	//copy back
-	float* copiedBack = new float[height * width];
-	DeviceMatrixCL_copyFromDevice(*dmpCL, copiedBack);
-
-	//verify
-	for (int i = 0; i < exampleImage.size().area(); i++)
-	{
-		assert(copiedBack[i] == f_imData[i]);
-	}
-	
 	return 0;
 }
