2e1c406371a96d50ad756a66f0224d3551fafd90
Mert Dikmen <mertdikmen@gmail.com>
2012-12-05 13:19:43 -0600

    opencv example for flexible filter

--- windows/test_flexiblefilter/main.cpp
+++ windows/test_flexiblefilter/main.cpp
@@ -4,7 +4,34 @@
 #include <iostream>
 #include <fstream>
 
+#include "omp.h"
+
 static char* exampleImagePath = "..\\..\\..\\media\\kewell1.jpg";
+
+void dump_output(char* file_name, float* data, const int channels, const int height, const int width)
+{
+	std::ofstream test_out(file_name, std::ios_base::out);
+
+	int index = 0;
+	for (int k = 0; k < channels; k++){
+		for (int j = 0; j < height; j++)
+		{
+			for (int i = 0; i < width; i++)
+			{
+				test_out << data[index++] << ", ";
+			}
+
+			test_out << std::endl;
+		}
+		test_out << "----------------------" << std::endl;
+	}
+	test_out.close();
+}
+
+#define DO_C
+#define DO_CUDA
+#define DO_OPENCL
+#define DO_OPENCV
 
 int main(int argc, char* argv[])
 {
@@ -30,76 +57,95 @@ int main(int argc, char* argv[])
 		filter_bank[i] = float( std::rand() ) / RAND_MAX;
 	}
 
+	double tic, toc;
+
+#ifdef DO_OPENCV
+	cv::Mat out_temp(height, width, CV_32FC1);
+	cv::Mat out_index(height, width, CV_32FC1);
+	cv::Mat out_value;
+
+	tic = omp_get_wtime();
+	for (int i = 0; i < num_filters; i++)
+	{
+		cv::Mat filter(filter_dim, filter_dim, CV_32FC1, filter_bank + i * filter_dim * filter_dim);
+
+		cv::matchTemplate(exampleImage, filter, out_temp, CV_TM_CCORR);
+
+		out_temp = cv::abs(out_temp);
+
+		if (i==0)
+		{
+			out_value = cv::abs(out_temp);
+		}
+		else 
+		{
+			out_value = cv::max(out_value, out_temp);
+		}
+
+	}
+	toc = omp_get_wtime();
+
+	std::cout << "OpenCV time: " << toc - tic << std::endl;
+
+	//dump_output("test_ff_opencv.out", (float*)out_value.data, 1, height, width);
+#endif
+
+#ifdef DO_C
 	//C Reference
 	float* retvalC = new float[2 * height * width];
+	
+	tic = omp_get_wtime();
 	cosine_filter(f_imData, filter_bank, height, width, filter_dim, filter_dim, num_filters, retvalC);
+	toc = omp_get_wtime();
 
+	std::cout << "C time: " << toc - tic << std::endl;
+
+	dump_output("test_ff_c.out", retvalC, 2, height, width);
+	delete[] retvalC;
+#endif
+
+#ifdef DO_CUDA
 	//CUDA Reference
-/*	DeviceMatrix::Ptr dmpCU = makeDeviceMatrix(height, width);
+	DeviceMatrix::Ptr dmpCU = makeDeviceMatrix(height, width);
+	
+	tic = omp_get_wtime();
+	
 	DeviceMatrix_copyToDevice(*dmpCU, f_imData);
 	set_filter_bank_cuda(filter_bank, num_filters * filter_dim * filter_dim);
 	DeviceMatrix3D::Ptr retdmCU = filter_frame_cuda_3(dmpCU, num_filters, 1, FF_OPTYPE_COSINE);
 	float* retvalCU = new float[height * width * 2];
 	DeviceMatrix3D_copyFromDevice(*retdmCU, retvalCU);
 
-	std::ofstream test_out("test.out", std::ios_base::out);
-	for (int j = 0; j < height; j++)
-	{
-		for (int i = 0; i < width; i++)
-		{
-			test_out << retvalCU[j * width + i] << ", ";
-		}
+	toc = omp_get_wtime();
 
-		test_out << std::endl;
-	}
+	std::cout << "CUDA time: " << toc - tic << std::endl;
 
-	test_out << std::endl << std::endl << std::endl;
+	dump_output("test_ff_cuda.out", retvalCU, 2, height, width);
+	delete[] retvalCU;
+#endif
 
-	for (int j = 0; j < height; j++)
-	{
-		for (int i = 0; i < width; i++)
-		{
-			test_out << retvalCU[height * width + j * width + i] << ", ";
-		}
-		test_out << std::endl;
-	}
-	test_out.close();
-	*/
+#ifdef DO_OPENCL
 	//OPENCL Reference
 	DeviceMatrixCL::Ptr dmpCL = makeDeviceMatrixCL(height, width);
+
+	tic = omp_get_wtime();
+
 	DeviceMatrixCL_copyToDevice(*dmpCL, f_imData);
 	set_filter_bank_cl(filter_bank, num_filters * filter_dim * filter_dim);
 	DeviceMatrixCL3D::Ptr retdm = filter_frame_cl_3(dmpCL, num_filters, 1, FF_OPTYPE_COSINE);
-	float* retval = new float[height * width * 2];
-	DeviceMatrixCL3D_copyFromDevice(*retdm, retval);
+	float* retvalCL = new float[height * width * 2];
+	DeviceMatrixCL3D_copyFromDevice(*retdm, retvalCL);
 
-	std::ofstream test_out_cl("testcl.out", std::ios_base::out);
-	for (int j = 0; j < height; j++)
-	{
-		for (int i = 0; i < width; i++)
-		{
-			test_out_cl << retval[j * width + i] << ", ";
-		}
+	toc = omp_get_wtime();
 
-		test_out_cl << std::endl;
-	}
+	std::cout << "OpenCL time: " << toc - tic << std::endl;
 
-	test_out_cl << std::endl << std::endl << std::endl;
+	dump_output("test_ff_opencl.out", retvalCL, 2, height, width);
 
-	for (int j = 0; j < height; j++)
-	{
-		for (int i = 0; i < width; i++)
-		{
-			test_out_cl << retval[height * width + j * width + i] << ", ";
-		}
-		test_out_cl << std::endl;
-	}
-	test_out_cl.close();
+	delete[] retvalCL;
+#endif
 
 	delete[] filter_bank;
-	delete[] retval;
-	delete[] retvalC;
-	//delete[] retvalCU;
 
 	return 0;
 }
