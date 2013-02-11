ee0a684d387359ee9ae3a8f667dac98182f521ef
Mert Dikmen <mertdikmen@gmail.com>
2013-02-10 23:37:16 -0600

    file rename

--- test/cpp/test_pipeline/test_pipeline.cpp
+++ test/cpp/test_pipeline/test_pipeline.cpp
@@ -3,116 +3,14 @@
 #include "omp.h"
 #include <windows.h>
 
+#include "filterbank.hpp"
+#include "classifier.hpp"
+
 /*	which device to use
 	CPU 0
 	GPU 1
 */
 int device_use; 
-
-class FilterBank
-{
-public:
-	FilterBank(const int ndim, const int nfilters):
-	  n_filters(nfilters), n_dim(ndim)
-	  {
-		  data = new float[n_filters * n_dim * n_dim];
-		  for (int i = 0; i < n_filters * n_dim * n_dim; i++)
-		  {
-			  data[i] = float( std::rand() ) / RAND_MAX;
-		  }
-	  };
-
-	  //TODO: Read from file sFilterBank(const string file_name){};
-
-	  DeviceMatrixCL3D::Ptr apply_cl(DeviceMatrixCL::Ptr dmpCL)
-	  {
-		//  double tic0= omp_get_wtime();
-		  DeviceMatrixCL3D::Ptr retdm = filter_frame_cl_3(dmpCL, n_filters, 1, FF_OPTYPE_COSINE);
-	//	  double tic1= omp_get_wtime();
-		//  std::cout << "---filter outside time: " << tic1 - tic0 << std::endl;
-		  return retdm;
-	  };
-
-	  void set_on_device()
-	  {
-		  set_filter_bank_cl(data, n_filters * n_dim * n_dim);
-	  };
-
-	  ~FilterBank()
-	  {
-		  if (data!=NULL)
-		  {
-			  delete[] data;
-			  data = NULL;
-		  }
-	  }
-
-	  float* data;
-
-private:
-	int n_dim;
-	int n_filters;
-};
-
-class Classifier
-{
-public:
-	Classifier(const int window_height, const int window_width, const int cell_size, const int block_size, const int dict_size):
-	  _window_height(window_height), _window_width(window_width), _dict_size(dict_size),
-		  _cell_size(cell_size), _block_size(block_size)
-	  {
-		  _n_cells_x = _window_width / cell_size;
-		  _n_cells_y = _window_height / cell_size;
-
-		  _n_blocks_x = _n_cells_x - _block_size + 1;
-		  _n_blocks_y = _n_cells_y - _block_size + 1;
-
-		  _n_total_coeff = _block_size * _block_size * _n_blocks_x * _n_blocks_y, _dict_size;
-
-		  coefficients = new float[_n_total_coeff];
-
-		  classifierCL = makeDeviceMatrixCL(_n_total_coeff / _dict_size, _dict_size);
-
-		  for (int i = 0; i < _n_total_coeff; i++)
-		  {
-			  coefficients[i] = float( std::rand() ) / RAND_MAX;
-		  }
-
-		  DeviceMatrixCL_copyToDevice(*classifierCL, coefficients);
-	  };
-
-	  //TODO: read from file Classifier(const string file_name){};
-
-	  DeviceMatrixCL::Ptr apply(DeviceMatrixCL::Ptr blocks)
-	  {
-		  return pwdist_cl(classifierCL, blocks);
-	  };
-
-	  ~Classifier()
-	  {
-		  if (coefficients != NULL)
-		  {
-			  delete[] coefficients;
-			  coefficients = NULL;
-		  }
-	  };
-
-private:
-	const int _window_height;
-	const int _window_width;
-	const int _cell_size;
-	const int _block_size;
-	const int _dict_size;
-
-	int _n_total_coeff;
-
-	int _n_cells_x, _n_cells_y;
-	int _n_blocks_x, _n_blocks_y;
-
-	float* coefficients;
-
-	DeviceMatrixCL::Ptr classifierCL;
-};
 
 int main(int argc, char* argv[])
 {
