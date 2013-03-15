632848245343a690ebda8dbfe933c996a3776706
Mert Dikmen <mertdikmen@gmail.com>
2013-03-15 03:20:31 -0500

    opencv equivalents of kernels

--- test/cpp/common/classifier.hpp
+++ test/cpp/common/classifier.hpp
@@ -1,5 +1,66 @@
 #include "vivid.hpp"
 
+class ClassifierOCV
+{
+public:
+	ClassifierOCV(
+		const int window_height, const int window_width, 
+		const int cell_size, const int block_size, const int dict_size):
+	_window_height(window_height), _window_width(window_width), _dict_size(dict_size),
+		_cell_size(cell_size), _block_size(block_size)
+	{
+		_n_cells_x = _window_width / cell_size;
+		_n_cells_y = _window_height / cell_size;
+
+		_n_blocks_x = _n_cells_x - _block_size + 1;
+		_n_blocks_y = _n_cells_y - _block_size + 1;
+
+		_n_total_coeff = _block_size * _block_size * _n_blocks_x * _n_blocks_y * _dict_size;
+
+		coefficients = new float[_n_total_coeff];
+
+		for (int i = 0; i < _n_total_coeff; i++)
+		{
+			coefficients[i] = float( std::rand() ) / RAND_MAX;
+		}
+
+		classifierCV = cv::Mat(_n_total_coeff / _dict_size, _dict_size, CV_32FC1, coefficients);
+	}
+
+	cv::Mat apply(cv::Mat& input)
+	{
+		cv::Mat output(input.rows, classifierCV.rows, CV_32FC1);
+
+		float* output_data = (float*) output.data;
+
+		for (int i = 0; i < input.rows; i++)
+		{
+			for (int j = 0; j < classifierCV.rows; j++)
+			{
+				//std::cout << i << "\t" << j << std::endl;
+				cv::Mat diff = classifierCV.row(j) - input.row(i);
+				output_data[i * output.cols + j] = cv::norm(diff);				
+			}
+		}
+
+		return output;
+	}
+
+public:
+	const int _window_height;
+	const int _window_width;
+	const int _cell_size;
+	const int _block_size;
+	const int _dict_size;
+	int _n_total_coeff;
+	int _n_cells_x, _n_cells_y;
+	int _n_blocks_x, _n_blocks_y;
+
+	float* coefficients;
+
+	cv::Mat classifierCV;
+};
+
 class Classifier
 {
 public:
@@ -7,49 +68,49 @@ public:
 		const int window_height, const int window_width, 
 		const int cell_size, const int block_size, const int dict_size, 
 		vivid::DeviceType device_type):
-	  _window_height(window_height), _window_width(window_width), _dict_size(dict_size),
-		  _cell_size(cell_size), _block_size(block_size)
-	  {
-		  _n_cells_x = _window_width / cell_size;
-		  _n_cells_y = _window_height / cell_size;
+	_window_height(window_height), _window_width(window_width), _dict_size(dict_size),
+		_cell_size(cell_size), _block_size(block_size)
+	{
+		_n_cells_x = _window_width / cell_size;
+		_n_cells_y = _window_height / cell_size;
 
-		  _n_blocks_x = _n_cells_x - _block_size + 1;
-		  _n_blocks_y = _n_cells_y - _block_size + 1;
+		_n_blocks_x = _n_cells_x - _block_size + 1;
+		_n_blocks_y = _n_cells_y - _block_size + 1;
 
-		  _n_total_coeff = _block_size * _block_size * _n_blocks_x * _n_blocks_y * _dict_size;
+		_n_total_coeff = _block_size * _block_size * _n_blocks_x * _n_blocks_y * _dict_size;
 
-		  coefficients = new float[_n_total_coeff];
+		coefficients = new float[_n_total_coeff];
 
-		  classifierCL = makeDeviceMatrixCL(_n_total_coeff / _dict_size, _dict_size, device_type);
+		classifierCL = makeDeviceMatrixCL(_n_total_coeff / _dict_size, _dict_size, device_type);
 
-		  for (int i = 0; i < _n_total_coeff; i++)
-		  {
-			  coefficients[i] = float( std::rand() ) / RAND_MAX;
-		  }
+		for (int i = 0; i < _n_total_coeff; i++)
+		{
+			coefficients[i] = float( std::rand() ) / RAND_MAX;
+		}
 
-		  DeviceMatrixCL_copyToDevice(*classifierCL, coefficients);
+		DeviceMatrixCL_copyToDevice(*classifierCL, coefficients);
 
-		  result = makeDeviceMatrixCL(1,1,device_type);
-	  };
+		result = makeDeviceMatrixCL(1,1,device_type);
+	};
 
-	  //TODO: read from file Classifier(const string file_name){};
+	//TODO: read from file Classifier(const string file_name){};
 
-	  DeviceMatrixCL::Ptr apply(DeviceMatrixCL::Ptr blocks)
-	  {
-		  pwdist_cl(classifierCL, blocks, result);
+	DeviceMatrixCL::Ptr apply(DeviceMatrixCL::Ptr blocks)
+	{
+		pwdist_cl(classifierCL, blocks, result);
 
-		  return result;
-	  };
+		return result;
+	};
 
-	  ~Classifier()
-	  {
-		  if (coefficients != NULL)
-		  {
-			  delete[] coefficients;
-			  coefficients = NULL;
-		  }
-	  };
-	
+	~Classifier()
+	{
+		if (coefficients != NULL)
+		{
+			delete[] coefficients;
+			coefficients = NULL;
+		}
+	};
+
 	DeviceMatrixCL::Ptr classifierCL;
 private:
 	const int _window_height;
--- test/cpp/common/filterbank.hpp
+++ test/cpp/common/filterbank.hpp
@@ -1,4 +1,62 @@
+#include <iostream>
 #include "vivid.hpp"
+
+class FilterBankOCV
+{
+public:
+	FilterBankOCV(const int ndim, const int nfilters):
+		n_filters(nfilters), n_dim(ndim)
+	{
+		std::cout << n_filters << std::endl;
+		std::cout << n_dim << std::endl;
+		data = new float[n_filters * n_dim * n_dim];
+		for (int i = 0; i < n_filters * n_dim * n_dim; i++)
+		{
+			data[i] = float( std::rand() ) / RAND_MAX;
+		}
+
+		for (int i = 0; i < n_filters; i++)
+		{
+			filters.push_back(cv::Mat(n_dim, n_dim, CV_32FC1, data + i * (n_dim * n_dim)));
+		}
+	}
+
+	~FilterBankOCV()
+	{
+		delete data;
+	}
+
+	cv::Mat apply(cv::Mat& input)
+	{
+		cv::Mat max_val = cv::Mat::zeros(input.size(), input.type());
+		cv::Mat max_ind = cv::Mat::zeros(input.size(), input.type());
+		cv::Mat temp_result(input.size(), input.type());
+		for (int i = 0; i < n_filters; i++)
+		{
+			cv::filter2D(input, temp_result, CV_32F, filters[i]);
+			//cv::matchTemplate(input, filters[i], temp_result, CV_TM_CCORR);
+			temp_result = cv::abs(temp_result);
+			for (int ii = 0; ii < temp_result.size().area(); ii++)
+			{
+				if (temp_result.data[ii] > max_val.data[ii])
+				{
+					max_val.data[ii] = temp_result.data[ii];
+					max_ind.data[ii] = ii;
+				}
+			}
+		}
+
+		return max_val;
+	}
+
+	float* data;
+
+private:
+	int n_dim;
+	int n_filters;
+
+	std::vector<cv::Mat> filters;
+};
 
 class FilterBank
 {
