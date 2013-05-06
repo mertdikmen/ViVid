f46bae92d2e975576298d8b03224664a2a50e51d
Mert Dikmen <mertdikmen@gmail.com>
2013-05-06 14:54:57 -0500

    include guards

--- src/OpenCLTypes.hpp
+++ src/OpenCLTypes.hpp
@@ -1,6 +1,9 @@
-#pragma once
+#ifndef OPENCL_TYPES_H
+#define OPENCL_TYPES_H
 
 namespace vivid
 {
 	enum DeviceType { DEVICE_GPU = CL_DEVICE_TYPE_GPU, DEVICE_CPU = CL_DEVICE_TYPE_CPU };
 }
+
+#endif
